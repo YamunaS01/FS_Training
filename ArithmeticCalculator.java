b418fce932bca255289b4a6aaaffb777179d40ea
unknown <yamunasivasan01@gmail.com>
2021-06-06 01:43:14 +0530

    Add the programs

---
+++ ArithmeticCalculator.java
@@ -0,0 +1,16 @@
+
+public class ArithmeticCalculator {
+
+	public static void main(String[] args) {
+	int a=10; int b=30; int c; int d;
+	c = a+b;
+	d = b-a;
+	System.out.println("Arithmetic Calculator");
+	System.out.println("Addition of numbers = "+ c);
+	System.out.println("Multiplication of numbers = "+ a*b);
+	System.out.println("Division of numbers = "+ a/b);
+	System.out.println("Subtraction of numbers = "+ d);
+	}
+
+}
+ 
